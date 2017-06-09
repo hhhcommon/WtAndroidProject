5dfd9f1ac31121f4ffb07daa4f23e1fad26e1630
54PirateGit <18179739967@163.com>
2017-06-09 18:45:48 +0800

    commit  0609

---
+++ hardware/src/main/java/com/wotingfm/ui/test/mine/fragment/FeedbackFragment.java
@@ -0,0 +1,59 @@
+package com.wotingfm.ui.test.mine.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wotingfm.R;
+import com.wotingfm.ui.test.mine.MineActivity;
+
+/**
+ * 意见反馈
+ * Created by Administrator on 2017/6/9.
+ */
+public class FeedbackFragment extends Fragment implements View.OnClickListener {
+    private View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_feedback, container, false);
+            rootView.setOnClickListener(this);
+
+            initView();
+            initEvent();
+        }
+        return rootView;
+    }
+
+    // 初始化视图
+    private void initView() {
+
+    }
+
+    // 初始化点击事件
+    private void initEvent() {
+        rootView.findViewById(R.id.head_left_btn).setOnClickListener(this);// 返回
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (rootView != null) {
+            ((ViewGroup) rootView.getParent()).removeView(rootView);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.head_left_btn:// 返回
+                MineActivity.close();
+                break;
+        }
+    }
+}
---
+++ hardware/src/main/java/com/wotingfm/ui/test/mine/fragment/MessageSettingFragment.java
@@ -0,0 +1,50 @@
+package com.wotingfm.ui.test.mine.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wotingfm.R;
+
+/**
+ * 消息设置
+ * Created by Administrator on 2017/6/9.
+ */
+public class MessageSettingFragment extends Fragment {
+    private View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_message_setting, container, false);
+
+            initView();
+            initEvent();
+        }
+        return rootView;
+    }
+
+    // 初始化视图
+    private void initView() {
+        TextView textTitle = (TextView) rootView.findViewById(R.id.tv_center);// 标题
+        textTitle.setText(getString(R.string.message_settings));
+    }
+
+    // 初始化点击事件
+    private void initEvent() {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (rootView != null) {
+            ((ViewGroup) rootView.getParent()).removeView(rootView);
+        }
+    }
+}
