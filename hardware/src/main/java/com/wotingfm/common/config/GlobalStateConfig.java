5760e433c37242af961d9a963d70de3447c17f04
苹果丸子 <645700751@qq.com>
2017-06-30 11:27:58 +0800

    对讲

--- hardware/src/main/java/com/wotingfm/common/config/GlobalStateConfig.java
+++ hardware/src/main/java/com/wotingfm/common/config/GlobalStateConfig.java
@@ -14,7 +14,7 @@ public class GlobalStateConfig {
     /**
      * 是否是测试代码
      */
-    public static final boolean test = false;
+    public static final boolean test = true;
     /**
      * 用于档位切换的参数
      * A：播放模块
