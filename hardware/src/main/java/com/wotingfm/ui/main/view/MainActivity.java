9b8c47a9483f3ebbe5250ed6e542219b8de69062
_Amine <gongming158@163.com>
2017-08-17 11:58:06 +0800

    谁在说话

--- hardware/src/main/java/com/wotingfm/ui/main/view/MainActivity.java
+++ hardware/src/main/java/com/wotingfm/ui/main/view/MainActivity.java
@@ -1,14 +1,18 @@
 package com.wotingfm.ui.main.view;
 
+import android.Manifest;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -31,6 +35,7 @@ import com.wotingfm.common.constant.BroadcastConstants;
 import com.wotingfm.common.service.InterPhoneControl;
 import com.wotingfm.common.service.NotificationService;
 import com.wotingfm.common.service.WtDeviceControl;
+import com.wotingfm.common.utils.T;
 import com.wotingfm.ui.intercom.main.chat.presenter.ChatPresenter;
 import com.wotingfm.ui.intercom.main.view.InterPhoneActivity;
 import com.wotingfm.ui.main.presenter.MainPresenter;
@@ -93,7 +98,7 @@ public class MainActivity extends TabActivity implements View.OnClickListener {
         // 从assets目录下面的加载html
         // mWebView.loadUrl("https://rtcmulticonnection.herokuapp.com/demos/Audio-Conferencing.html?roomid=123456789");
         mWebView.loadUrl("https://apprtc.wotingfm.com/demos/Audio-Conferencing.html?simple=true");
-        mWebView.addJavascriptInterface(new WebViewCall(), "android");
+        mWebView.addJavascriptInterface(this, "android");
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
@@ -338,18 +343,20 @@ public class MainActivity extends TabActivity implements View.OnClickListener {
 
     private Handler handler = new Handler(Looper.myLooper());
 
-    public class WebViewCall {
-        @JavascriptInterface
-        public void beginSpeakCallBack(final String userId, final String username, final String useravatar) {
-            if (handler != null)
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.i("mingku", "userId=" + userId + ":" + username + ":" + useravatar);
-                    }
-                });
-        }
+    /**
+     * Java方法，谁在说话
+     */
+    @JavascriptInterface
+    public void beginSpeakCallBack(final String userId, final String username, final String useravatar, final String roomNumber) {
+        if (handler != null)
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("mingku", "userId=" + userId + ":" + username + ":" + useravatar + ":" + roomNumber);
+                }
+            });
     }
+
 
     /**
      * 退出房间
